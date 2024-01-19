f67546ec9456a7285760d3e0611418f6e786f7c8
endulum <71853916+endulum@users.noreply.github.com>
2024-01-19 11:52:51 -0800

    basic tests for checking guesses

--- tests/index.test.js
+++ tests/index.test.js
@@ -12,7 +12,7 @@ beforeAll(async () => {
 
 app.use('/', router);
 
-test('index route works', done => {
+test.only('index route works', done => {
   request(app)
     .get('/')
     .expect('Content-Type', /json/)
@@ -20,5 +20,54 @@ test('index route works', done => {
     .expect(200, done)
 });
 
-// todo: resolve testing warning
-// "A worker process has failed to exit gracefully and has been force exited."
+describe('sending guesses and receiving feedback', () => {
+  test('incomplete guess', done => {
+    request(app)
+      .post('/maps/1')
+      .type('form')
+      .send({})
+      .expect([
+        {
+          path: 'x',
+          msg: 'X-coordinate is missing.'
+        }, {
+          path: 'y',
+          msg: 'Y-coordinate is missing.'
+        }, {
+          path: 'character',
+          msg: 'No character was specified.'
+        }
+      ])
+      .expect(200, done)
+  });
+
+  test('wrong guess', done => {
+    request(app)
+      .post('/maps/1')
+      .type('form')
+      .send({
+        x: 0,
+        y: 0,
+        character: 'Waldo'
+      })
+      .expect({
+        isCorrect: false
+      })
+      .expect(200, done)
+  });
+
+  test('correct guess', done => {
+    request(app)
+      .post('/maps/1')
+      .type('form')
+      .send({
+        x: 1377,
+        y: 650,
+        character: 'Waldo'
+      })
+      .expect({
+        isCorrect: true
+      })
+      .expect(200, done)
+  });
+});
